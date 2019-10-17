1df85425b3cea4a9888c9aad493d74934602d567
Julian Edward Missiona <julian.edward.missiona@codeandtheory.com>
2019-10-17 14:51:54 +0800

    JULIAN'S COMMIT

--- tests/test_new_project_login.py
+++ tests/test_new_project_login.py
@@ -11,3 +11,5 @@ class TestProjectLogin(object):
         #on.LoginPage.login_as_standard_user(self)
         on.LoginPage.search_test(self)
         on.LoginPage.screenshot(self)
+
+        #RicardoMilosNumbah1
