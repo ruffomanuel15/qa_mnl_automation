7b87ddfbd3c2ad952a1b131aea3dbcbfeba51ed8
erika-bonganay <erika.bonganay@codeandtheory>
2019-10-17 15:50:22 +0800

    Config files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/raphaelmanuel/qa_mnl_automation/venv"
+set -gx VIRTUAL_ENV "/Users/erikabonganay/qa_mnl_automation/venv"
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | tr "." "\n")[1] -lt 3
