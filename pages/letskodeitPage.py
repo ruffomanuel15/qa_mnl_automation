741e8e2f074dbe16289e30edc9ea7a870d933f77
erika-bonganay <erika.bonganay@codeandtheory>
2019-10-21 11:14:48 +0800

    Assert Remaining elements in the table - Erika

--- pages/letskodeitPage.py
+++ pages/letskodeitPage.py
@@ -1,6 +1,7 @@
 from locators.page_elements import *
 from utils import environment as env
 from extensions.commands import *
+from selenium.webdriver.common.by import By
 
 
 class BasePage(object):
@@ -14,7 +15,21 @@ class LetsKodeItPage(BasePage):
 
     def assertion_tests(self):
         table = self.driver.find_element(*LetsKodeitSelectors.TABLE)
-
         assert table.is_displayed(), 'Table is missing'
 
-        #TODO assert the remaining elements in the table
+        # TODO assert the remaining elements in the table
+
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        for row in rows:
+            header = row.find_elements(*LetsKodeitSelectors.TH)
+            # for th in header:
+            #     print(th.text)
+            #     assert th.is_displayed(), 'Header is missing'
+            col = row.find_elements(*LetsKodeitSelectors.TD)
+            # for td in col:
+            #     print(td.text)
+            #     assert td.is_displayed(), 'Data is missing'
+            assert header is not None, 'Header is missing'
+            assert col is not None, 'Data is missing'
+
+
