277b08271bddc7393deed7b565bebfe51088d29d
Julian Edward Missiona <julian.edward.missiona@codeandtheory.com>
2019-10-25 15:55:05 +0800

    Julian - Training Challenge 3

--- pages/letskodeitPage.py
+++ pages/letskodeitPage.py
@@ -19,3 +19,30 @@ class LetsKodeItPage(BasePage):
         assert table.is_displayed(), 'Table is missing'
 
         # TODO assert the remaining elements in the tabl
+
+        #Julian Challenge 3
+
+        #Get Rows#
+        rows = table.find_elements(*LetsKodeitSelectors.TR)
+
+        #Get Headers
+
+
+        #Check Items Per Row
+        for row in rows:
+            headers = row.find_elements(*LetsKodeitSelectors.TH)
+            data = row.find_elements(*LetsKodeitSelectors.TD)
+
+
+            #Print Headers and Rows
+            for header in headers:
+                print(header.text)
+
+            for item in data:
+                print(item.text)
+
+            #Assertion Per Row Checked
+            assert headers is not None, 'Table Header element is not displayed'
+            assert data is not None, 'Table Header element is not displayed'
+
+
