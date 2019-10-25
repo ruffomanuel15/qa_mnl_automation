5040c7a079f73a42b363d755ee4c50328bbc7c27
Raphael Manuel <raphael.manuel@codeandtheory.com>
2019-10-25 11:00:49 +0800

    removed letskodeit answers

--- pages/letskodeitPage.py
+++ pages/letskodeitPage.py
@@ -18,17 +18,4 @@ class LetsKodeItPage(BasePage):
         table = self.driver.find_element(*LetsKodeitSelectors.TABLE)
         assert table.is_displayed(), 'Table is missing'
 
-        # TODO assert the remaining elements in the table
-
-        rows = table.find_elements(*LetsKodeitSelectors.TR)
-        for row in rows:
-            header = row.find_elements(*LetsKodeitSelectors.TH)
-            # th header
-            # to print (th.text)
-            # assert th.is displayed(), 'Header element not found'
-            col = row.find_elements(*LetsKodeitSelectors.TD)
-            # td col
-            # to print (td.text)
-            # assert td.displayed(), 'Data element not found'
-            assert header is not None, 'Header element not found'
-            assert col is not None, 'Data element not found'
+        # TODO assert the remaining elements in the tabl
